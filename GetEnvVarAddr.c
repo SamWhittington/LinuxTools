b5883eca2d1f39b2ce29e81a4d33d878fba160cd
Sam Whittington <69531345+SamWhittington@users.noreply.github.com>
2020-12-16 22:43:59 +0000

    Update GetEnvVarAddr.c

--- GetEnvVarAddr.c
+++ GetEnvVarAddr.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) {
 
 	char *ptr;
 
 	ptr = getenv(argv[1]; /* This gets the env variable location */
 	ptr += (strlen(argv[0]) - strlen(argv[2]))*2l /* Ajust length for the program name */ 
 	printf("%s address: %p\n", argc[1], ptr);
+}
